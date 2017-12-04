0e8fa880837f4e50473e24183766015d939771c5
Wheatley <Wheatley@MacBook-Pro.local>
2017-12-04 12:40:46 -0700

    phew

--- src/components/FamilyChanger.js
+++ src/components/FamilyChanger.js
@@ -4,15 +4,20 @@ export default class FamilyChanger extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      allowEdit: this.props.allowEdit
+      allowEdit: props.allowEdit
     };
   }
 
-  // componentWillReceiveProps
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      allowEdit : nextProps.allowEdit
+    })
+  }
 
   render() {
     return (
-      <select className="dropDownContainer">
+      <select className="dropDownContainer" onChange={e=>this.props.update(e.target.value)}
+        disabled={this.state.allowEdit==="false"}>
         <option value="monospace"> Monospace </option>
         <option value="arial"> Arial </option>
         <option value="courier"> Courier </option>
